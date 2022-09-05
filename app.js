15ae170bac421798307631fa943bd58b9bb1157b
mgdaghero <marcelo.daghero@cencosud.com>
2022-09-05 14:28:56 -0300

    commit rama Marce

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ const app = express();
 app.listen(PORT, () => {
     console.log(`Server listen in ${PORT}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+app.get('/berners-lee', (req, resp) => {
+    resp.sendFile(path.join(__dirname, '/views/berners-lee'));
+})
+
+app.get('/clarke', (req, resp) => {
+    resp.sendFile(path.join(__dirname, '/views/berners-lee'));
+})
