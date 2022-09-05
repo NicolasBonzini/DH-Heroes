44ade46ef8a15166a3967b1b4567317f6431c9df
santiago_del_rivero <santiago.delrivero@cencosud.com>
2022-09-05 14:27:30 -0300

    parte santi

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const PORT = 3030;
 
 const app = express();
 
+app.get('/hamilton', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/hamilton.html'))
+})
+
+app.get('/hopper', (req,res) =>{
+    res.sendFile(path.join(__dirname, '/views/hopper.html'))
+})
+
 app.listen(PORT, () => {
     console.log(`Server listen in ${PORT}`);
 })
