770afa0e0830e5aa2453dd29b35dd83d8678641b
PabloCamejo <pablo.camejo@cencosud.com>
2022-09-05 14:14:07 -0300

    commit

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const PORT = 3030;
 
 const app = express();
 
+app.get('/lovelace', (req, res) => {
+    res.sendFile(__filename,);
+});
+
+app.get('/turing', (req, res) => {
+    ;
+})
+
 app.listen(PORT, () => {
     console.log(`Server listen in ${PORT}`);
 })
